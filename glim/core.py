b71bba2c575101396d2cd99dd069a11f1c2d6382
Nauman Ahmad <nauman-ahmad@outlook.com>
2014-12-28 02:06:20 +0500

    Improved Readability

--- glim/core.py
+++ glim/core.py
@@ -1,22 +1,25 @@
 """
-        _ _           
-   __ _| (_)_ __ ___  
-  / _` | | | '_ ` _ \ 
+        _ _
+   __ _| (_)_ __ ___
+  / _` | | | '_ ` _ \
  | (_| | | | | | | | |
   \__, |_|_|_| |_| |_|
-  |___/  
+  |___/
 
 author: Nauman Ahmad
 description: An API endpoint to quickly resize images.
 version: 0.1
 license: MIT
 """
+# Dependencies
 from glim import app
 from flask import render_template
 from PIL import Image
+import requests
+
+# Standard Library Imports
 from urlparse import urlparse
 import base64
-import requests
 import StringIO
 import cStringIO
 import imghdr
@@ -32,6 +35,7 @@ END_POINT = 'https://api.imgur.com/3/upload.json'
 def _protocol(url):
     """Checks if http:// is present before Url"""
     parsed = urlparse(url)
+
     if parsed.scheme == "":
         return "http://" + url
     else:
@@ -41,36 +45,46 @@ def _protocol(url):
 def _get_image(link):
     """Get image from a link"""
     link = _protocol(link)
+
     try:
         fp = requests.get(link)
     except requests.exceptions.ConnectionError:
         return False
+
     img = StringIO.StringIO(fp.content)
     return img
 
 
 def _get_ext(img):
-    """Get Image Extension"""
+    """
+    Get Image Extension
+    """
     image_ext = imghdr.what(img)
     return image_ext
 
 
 def _resize_image(height, width, img, ext):
-    """Resize the image from the link"""
+    """
+    Resize the image from the link
+    """
     try:
         image = Image.open(img)
         im2 = image.resize((height, width), Image.ANTIALIAS)
     except:
         return False
+
     # Save image temp in memory using StringIO
     temp = cStringIO.StringIO()
     im2.save(temp, ext)
     temp.seek(0)
+
     return temp.getvalue()
 
 
 def _upload_image(img, name, ext):
-    """Upload resized image to Imgur"""
+    """
+    Upload resized image to Imgur
+    """
     headers = {"Authorization": "Client-ID 29619ae5d125ae6"}
     response_data = requests.post(
         END_POINT,
@@ -83,13 +97,17 @@ def _upload_image(img, name, ext):
             'title': name
         }
     )
+
     return response_data.json()['data']['link']
 
 
 def _random_string():
-    """Generates a random name for image"""
+    """
+    Random name generator
+    """
     length = 6
     rv = ""
+
     for i in range(length):
         rv += random.choice(string.ascii_lowercase)
     return rv
@@ -97,37 +115,47 @@ def _random_string():
 
 @app.errorhandler(404)
 def handle_error(e):
-    """Display the 404 template on error"""
+    """
+    Route: 404
+    """
     return render_template('404.html')
 
 
 @app.route('/')
-def main():
-    """Main routing function"""
+def index():
+    """
+    Route: Index
+    """
     return render_template('index.html')
 
 
 @app.route('/<string:size>/<path:url>')
 @app.route('/<string:size>/<string:return_type>/<path:url>')
 def api(size, url, return_type=None):
-    """Api controller of glim"""
+    """
+    Route: API Controller
+    """
     # TODO: Regex match size string
     if "x" not in size:
         err_size = "Size string invalid, {height}x{width}"
         return render_template("errors.html", error_msg=err_size)
     sizes = size.split('x')  # Get sizes from url paramater
+
+    # Size params integers?
     try:
         height = int(sizes[0])
         width = int(sizes[1])
     except ValueError:
         err_int = "Size string invalid, use integers only! eg 400x400"
         return render_template('errors.html', error_msg=err_int)
+
     # Generate unique name
     unq_name = _random_string()
     img = _get_image(url)
     if not img:
         err_link = "Invalid link, try again!"
         return render_template('errors.html', error_msg=err_link)
+
     # Get image extension
     ext = _get_ext(img)
     resized_image = _resize_image(height, width, img, ext)
@@ -135,8 +163,10 @@ def api(size, url, return_type=None):
         err_format = "Unsupported image format!"
         return render_template('errors.html', error_msg=err_format)
     img_link = _upload_image(resized_image, unq_name, ext)
+
+    # Return raw imgur link
     if return_type == "link":
-        # Return raw imgur link
         return "<a href='%s'>%s</a>" % (img_link, img_link)
+
     # Return image to webpage
     return "<img src='%s'></img>" % img_link
--- runserver.py
+++ runserver.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    # Cache template for fast loading
+    # for fast template loading
     app.jinja_env.cache = {}
     app.run(host='0.0.0.0', port=port, debug=True)
